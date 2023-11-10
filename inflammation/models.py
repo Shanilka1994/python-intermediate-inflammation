65bbade91f773362522630a1e3ce7e66738b76a8
Mayodi <mayodi.fernandopulle@ukaea.uk>
2023-11-10 13:30:41 +0000

    docstring improvements

--- inflammation/models.py
+++ inflammation/models.py
@@ -41,16 +41,25 @@ def load_json(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    :param data: a 2D array with inflammation data
+    :return: an array of mean value of measurements of each day
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    :param data: a 2D array with inflammation data
+    :return: an array of max value of measurements of each day  
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    :param data: a 2D array with inflammation data
+    :return: an array of min value of measurements of each day 
+    """
     return np.min(data, axis=0)
 
